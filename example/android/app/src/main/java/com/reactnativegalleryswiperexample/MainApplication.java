fa4152dd23623b0e1ff4c61c8a6467d2f98475a7
Lue <lue_hang@hotmail.com>
2018-12-21 10:51:14 -0600

    Updated example project.

--- example/android/app/src/main/java/com/reactnativegalleryswiperexample/MainApplication.java
+++ example/android/app/src/main/java/com/reactnativegalleryswiperexample/MainApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.reactnativegalleryswiperexample;
 
 import android.app.Application;
 
--- example/index.js
+++ example/index.js
@@ -1,5 +1,3 @@
-/** @format */
-
 import { AppRegistry } from "react-native";
 import ReactNativeGallerySwiperExample
     from "./ReactNativeGallerySwiperExample";
